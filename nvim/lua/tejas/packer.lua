bbe80fa138cb3f23363830e641b52043bc59ddba
Tejas G <ttejasgarrepally@gmail.com>
2023-01-12 19:55:45 +0800

    install lualine icons

--- nvim/lua/tejas/packer.lua
+++ nvim/lua/tejas/packer.lua
@@ -6,6 +6,12 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
     use 'wbthomason/packer.nvim'
+
+    use 'folke/tokyonight.nvim'
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
     
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
