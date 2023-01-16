d9171028418cec6e4168751dedef3087dbbc03fb
Tejas G <ttejasgarrepally@gmail.com>
2023-01-16 21:53:11 +0800

    added some lsp settings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,5 +1,50 @@
 local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
+
+lsp.ensure_installed({
+    'sumneko_lua',
+    'rust_analyzer'
+})
+
+-- Fix Undefined global 'vim'
+lsp.configure('sumneko_lua', {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = { 'vim' }
+            }
+        }
+    }
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+  ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ["<C-Space>"] = cmp.mapping.complete(),
+})
+
+lsp.setup_nvim_cmp({
+  mapping = cmp_mappings
+})
+
+lsp.set_preferences({
+    suggest_lsp_servers = false,
+    sign_icons = {
+        error = 'E',
+        warn = 'W',
+        hint = 'H',
+        info = 'I'
+    }
+})
+
+
 lsp.setup()
 -- Still have yet to customise fully
+
+vim.diagnostic.config({
+    virtual_text = true,
+})
