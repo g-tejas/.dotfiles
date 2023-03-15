222f3af30efc1a87594ec742d4c7995950ffd456
Tejas G <ttejasgarrepally@gmail.com>
2023-03-15 09:33:35 +0800

    some scripts for raycast

---
+++ _scripts/mindpalace.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Dependency: requires macOCR
+# Download: https://github.com/schappim/macOCR
+
+# @raycast.schemaVersion 1
+# @raycast.title MindPalace
+# @raycast.mode silent
+# @raycast.author Lim Chee Aun
+# @raycast.authorURL https://github.com/cheeaun
+# @raycast.description Get any text on your screen into your clipboard
+# @raycast.packageName macOCR
+# @raycast.icon üì∏
+
+python /Library/Mobile Documents/iCloud~md~obsidian/Documents/mindpalace/_scripts/obsidian_util.py
---
+++ _scripts/ocr.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Dependency: requires macOCR
+# Download: https://github.com/schappim/macOCR
+
+# @raycast.schemaVersion 1
+# @raycast.title macOCR
+# @raycast.mode silent
+# @raycast.author Lim Chee Aun
+# @raycast.authorURL https://github.com/cheeaun
+# @raycast.description Get any text on your screen into your clipboard
+# @raycast.packageName macOCR
+# @raycast.icon 📸
+
+ocr
